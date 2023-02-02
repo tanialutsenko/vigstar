bb692dada851f49042cedf485a0c01bcf3d4f5f0
tania <lutsenkovigstar@yandex.ru>
2023-02-02 15:11:02 +0300

    1 commit

--- pythonProject/website.py
+++ pythonProject/website.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 menu = ["установка", "первое приложение", "обратная связь"]
 @app.route("/")
 def index():
+    print(url_for('index'))
     return render_template('index.html', menu = menu )
 
 @app.route("/about")
 def about():
+    print(url_for('about'))
     return "<h1> О Flask </h1>"
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Пользователь{username}"
+
+# with app.test_request_context():
+#     print(url_for('about'))
+#     print(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
