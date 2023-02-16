d2fea426d9fc0d55a067a014aa38e0ec8ee973ed
tania <lutsenkovigstar@yandex.ru>
2023-02-16 12:53:28 +0300

    push website

--- pythonProject/website.py
+++ pythonProject/website.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'tryiu859jgnbcgfy74lh8d9j'
 
 menu = [{"name":"установка", "url": "install-flash"},
         {"name" :"первое приложение", "url": "first-app"},
         { "name" :"обратная связь", "url": "contact"}]
+
+# get_flash_messages = ['Сообщение отправлено','Ошибка отправления']
 
 @app.route("/")
 def index():
@@ -23,7 +26,10 @@ def profile(username):
 @app.route("/contact", methods = ["POST", "GET"])
 def contact():
     if request.method == "POST":
-        print(request.form)
+        if len(request.form['username'])>2:
+            flash('Сообщение отправлено',category = 'success')
+        else:
+            flash('Ошибка отправления', category = 'error')
     return render_template('contact.html', title = "Обратная связь",menu = menu)
 
 # with app.test_request_context():
@@ -31,6 +37,6 @@ def contact():
 #     print(url_for('index'))
 #     print(url_for('profile', username="self"))
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
